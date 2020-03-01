59812563b62db9b586c90a3287faa483153ca8c9
Lassi Paavolainen <lassi.paavolainen@gmail.com>
2020-03-01 15:14:06 +0100

    Added Dockerfile

--- wrapper.py
+++ wrapper.py
@@ -1,15 +1,17 @@
 import sys
 import os
 from cytomine.models import Job
-from neubiaswg5 import CLASS_OBJSEG
-from neubiaswg5.helpers import NeubiasJob, prepare_data, upload_data, upload_metrics
+from neubiaswg5 import CLASS_OBJSEG, CLASS_SPTCNT, CLASS_PIXCLA, CLASS_TRETRC, CLASS_LOOTRC, CLASS_OBJDET, CLASS_PRTTRK, CLASS_OBJTRK
+from neubiaswg5.helpers import NeubiasJob, prepare_data, upload_data, upload_metrics, get_discipline
 
 
 def main(argv):
     base_path = "{}".format(os.getenv("HOME")) # Mandatory for Singularity
-    problem_cls = CLASS_OBJSEG
-
+    
     with NeubiasJob.from_cli(argv) as nj:
+        # Change following to the actual problem class of the workflow
+        problem_cls = get_discipline(nj, default=CLASS_OBJSEG)
+        
         nj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
         
         # 1. Prepare data for workflow
@@ -17,6 +19,8 @@ def main(argv):
 
         # 2. Run image analysis workflow
         nj.job.update(progress=25, statusComment="Launching workflow...")
+
+        # Add here the code for running the analysis script
 
         # 3. Upload data to BIAFLOWS
         upload_data(problem_cls, nj, in_imgs, out_path, **nj.flags, monitor_params={
